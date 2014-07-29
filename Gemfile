0f8c1d38f85e1b4c1da89a11b1c9c8411cbd17f0
ryonext <ryonext.s@gmail.com>
2014-07-29 23:31:05 +0900

    Store redis uniq user

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "sinatra", require: 'sinatra/base'
+gem "redis"
+
+group :development do
+  gem "sinatra-contrib", require: 'sinatra/reloader'
+end
---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+require 'redis'
+
+post '/' do
+  body = request.body.read
+  param = JSON.parse(body)
+  user_id = param["user_id"]
+  redis = Redis.new
+  today = Time.now.strftime('%Y%m%d')
+  redis.sadd("launch_#{today}", user_id)
+end
